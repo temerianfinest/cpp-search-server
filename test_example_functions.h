62f9d6811b04784c522f3900283b2744f0cb8668
temerianfinest <126896272+temerianfinest@users.noreply.github.com>
2023-08-24 04:45:12 +0500

    Delete test_example_functions.h

--- test_example_functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include "search_server.h"
-#include "log_duration.h"
-#include "document.h"
-
-void AddDocument(SearchServer search_server,int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
